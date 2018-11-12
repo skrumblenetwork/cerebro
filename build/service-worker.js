57608d89a9dbd9a4b649feb4bdce2b17a6eb9225
Jonathan Marcello <j.marcello@skrumble.com>
2018-11-12 10:44:39 -0500

    Initial Git Pages commit

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ccb57d5ca5214dc8b4b8145149df7e0d.js"
+  "/cerebro/precache-manifest.1382e50681eb07779d40554792136e3b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/cerebro/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
